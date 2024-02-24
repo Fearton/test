e49dac7a13b30c1737e96ba4807cf7ac1feccddb
Fearton <wakkagi.nt@gmail.com>
2024-02-24 22:56:44 +0300

    Method to create array added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateArray (int size)
+{
+    string[] array = new string[size];
+    System.Console.WriteLine("Enter array: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine()!;
+    }
+    return array;
+}
+
+System.Console.WriteLine("Enter array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = CreateArray(size);
